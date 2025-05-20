b80f4985b09d583b6a3b08cde57fff65daa6f78f
jinnn12 <prohh12@naver.com>
2025-05-20 12:40:58 +0900

    user managing

--- 2_user_management.sh
+++ 2_user_management.sh
@@ -3,9 +3,16 @@
   sudo passwd newuser1
 
 # 사용자 변경 (이땐 newuser1의 비번을 침)
-# sudo의 경우 현재 사용자의 비밀번호호
-  su - newuser1 (su = switching user) 
+# sudo의 경우 현재 사용자의 비밀번호
+  su - newuser1 (su = switching user)
 
+# 권한 부여 (체인지 mod, r : 4 , w = 2 , x = 1, u(소유자)g(그룹)o(그외)로 구성)
+  chmod 644 파일명 (rw - r - r)
+  chmod 664 파일명 (rw - rw - r)
+
+  chmod u + x 파일명 (user 한테 x 실행권한 줌)
+  chmod u - x 파일명 (user 한테 x 실행권한 뺏음)
+  chmod u = r 파일명 (user 한테 r 권한 줌)
   
 
 
